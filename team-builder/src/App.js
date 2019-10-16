3cf1280b149dd9203ec12b6b6f6099bee85331b7
Nerissa Taylor <taylornerissa@hotmail.com>
2019-10-16 18:40:04 -0400

    still working on it

---
+++ team-builder/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  const [members, setMembers] = useState([]);
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/Form.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name"
+            />
+            <input
+                type="email"
+                id="Date.now()"
+                name="email"
+                placeholder="Email"
+            />
+            <input
+                type="role"
+                id="Date.now()"
+                name="role"
+                placeholder="Role"
+            />
+
+
+        </div>
+    )
+}
+
+export default Form
+
